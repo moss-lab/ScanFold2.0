d5ebd195d1dc1ac4dd019d7a977b38d6061e6b9f
Evelyn <31992045+EthanCC-1@users.noreply.github.com>
2024-03-13 14:17:41 -0500

    Update ScanFoldFunctions.py
    fixed temperature not being processed correctly when RNAfold is used as the folding algorithm

--- ScanFoldFunctions.py
+++ ScanFoldFunctions.py
@@ -1187,7 +1187,9 @@ def get_frag_feature_list(seq, step_size, window_size, algo, temperature):
         CGratio_list.append(CGratio)
         AUratio_list.append(AUratio)
         if algo == "rnafold":
-            fc = RNA.fold_compound(frag)
+            md = RNA.md()
+            md.temperature = temperature
+            fc = RNA.fold_compound(frag,md)
             fc.pf()
             (centroid, distance) = fc.centroid()
             ensemble_diversity = round(fc.mean_bp_distance(), 2)
